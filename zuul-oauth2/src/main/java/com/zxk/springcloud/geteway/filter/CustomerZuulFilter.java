e03cedbc9954ce4e0752d8ce34d47366ee5a6419
zhuxk <zhuxk@staff.chinabyte.com>
2019-06-26 11:18:08 +0800

    zuul 自定义ZuulFilter

--- zuul-oauth2/src/main/java/com/zxk/springcloud/geteway/filter/CustomerZuulFilter.java
+++ zuul-oauth2/src/main/java/com/zxk/springcloud/geteway/filter/CustomerZuulFilter.java
@@ -1,8 +1,15 @@
 package com.zxk.springcloud.geteway.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Date;
+import java.util.Enumeration;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
@@ -52,6 +59,33 @@ public class CustomerZuulFilter extends ZuulFilter {
     @Override
     public Object run() throws ZuulException {
         System.err.println("先OAuth2鉴权成功之后，才会到zuulFilter中执行");
+        // 登录校验逻辑。
+        // 1）获取Zuul提供的请求上下文对象
+        RequestContext ctx = RequestContext.getCurrentContext();
+        // 2) 从上下文中获取request对象
+        HttpServletRequest req = ctx.getRequest();
+        // 3) 从请求中获取token
+        String token = req.getParameter("access_token");
+        Enumeration p = req.getParameterNames();
+        while(p.hasMoreElements()){
+            String pname = (String)p.nextElement();
+            System.err.println("param: "+ pname + " = " + req.getParameter(pname));
+        }
+
+        Enumeration e = req.getAttributeNames();
+        while (e.hasMoreElements()){
+            String aname = (String)e.nextElement();
+            System.err.println("attribute: "+ aname + " = " + req.getAttribute(aname));
+        }
+
+        // 4) 判断
+        if(token == null || "".equals(token.trim())){
+            // 没有token，登录校验失败，拦截
+            ctx.setSendZuulResponse(false);
+            // 返回401状态码。也可以考虑重定向到登录页。
+            ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+        // 校验通过，可以考虑把用户信息放入上下文，继续向后执行
         return null;
     }
 }
