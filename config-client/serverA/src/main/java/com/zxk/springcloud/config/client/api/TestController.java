7da376f4a9d8b1f278eca8f9af5b54d2cce568ed
朱小康 <zhuxk@staff.chinabyte.com>
2019-08-27 17:03:52 +0800

    消息总线，热更新

--- config-client/serverA/src/main/java/com/zxk/springcloud/config/client/api/TestController.java
+++ config-client/serverA/src/main/java/com/zxk/springcloud/config/client/api/TestController.java
@@ -1,6 +1,7 @@
 package com.zxk.springcloud.config.client.api;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version: 1.0
  */
 @RestController
+/* 要实现刷新配置，需要配置@RefreshScope注释 */
+@RefreshScope
 public class TestController {
 
     @Value("${server.name}")
